435345a8141d118df8f13cee48522b9a72e1c5dd
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2019-10-20 14:53:50 +0200

    move blog to blog.php

--- navi.php
+++ navi.php
@@ -1,12 +1,12 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-primary mb-2">
+<nav class="navbar navbar-expand-md navbar-dark bg-primary">
 	<a class="navbar-brand" href="#">Play!</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
 	<div class="collapse navbar-collapse" id="navbarColor02">
 		<ul class="navbar-nav ml-auto">
-		<li class="nav-item">
-				<a class="nav-link" href=".">News</a>
+			<li class="nav-item">
+				<a class="nav-link" href="blog.php">Blog</a>
 			</li>
 			<li class="nav-item">
 				<a class="nav-link" href="downloads.php">Downloads</a>
