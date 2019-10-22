07a47a915d584338822bbdcfb82e71cb9c8f2a03
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2019-10-22 19:17:31 +0100

    update links.php page

--- links.php
+++ links.php
@@ -9,101 +9,60 @@
 	
 	<body>
 
-		<?php
+	<?php
 			include("navi.php");
 		?>
+		<div class="container-fluid">
 
-		<div>
-			<br />
-			<br />
+			<div class="row justify-content-center p-4">
+				<div class="col-12 col-lg-8">
+					<div class="jumbotron w-100 py-5 mx-auto">
+						<h1>Links</h1>
+						<p class="lead">
+							Various resources related to ps2, games, assets and developement are listed below. Some of these resources may no longer exist.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="row justify-content-center p-4">
+				<div class="col-12 col-md-10">
+
+					<div class="card w-100">
+						<div class="card-body">
+							<h5 class="card-title">PlayStation 2 resources </h5>
+							<p class="card-text">
+								<ul>
+									<li><a href="http://ps2dev.org">PS2DEV</a> - Great site for various PS2 hardware information and source code.</li>
+									<li><a href="http://www.pdroms.de/">PDRoms</a> - Nice collection of public domain ROM images.</li>
+								</ul>
+							</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="row justify-content-center p-4">
+					<div class="col-12 col-md-10">
+
+						<div class="card w-100">
+							<div class="card-body">
+								<h5 class="card-title">PSF Music Archives </h5>
+								<p class="card-text">
+									<ul>
+										<li><a href="http://tzone.org/~llin/psf/">PSF Mirror #1</a> - The ultimate PSF/PSF2 collection.</li>
+										<li><a href="http://www.zophar.net/music/psf.html">Zophar's Domain PSF Music Section</a> - Another very nice collection of PlayStation and PlayStation2 music.</li>
+									</ul>
+								</p>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
 		</div>
-
-		<table class="center">
-
-			<tr>
-
-				<td class="Header">
-
-					Links
-					
-				</td>
-
-			</tr>
-			
-			<tr>
-			
-				<td class="Title">
-				
-					Other Emulators
-				
-				</td>
-				
-			</tr>
-			
-			<tr>
-			
-				<td class="Log">
-				
-					<ul>
-					
-						<li><a href="http://www.pcsx2.net/">PCSX2</a> - Currently the best PS2 emulator around.</li>
-						
-					</ul>
-				
-				</td>
-				
-			</tr>
-
-			<tr>
-			
-				<td class="Title">
-				
-					PlayStation 2 resources
-					
-				</td>
-				
-			</tr>
-			
-			<tr>
-			
-				<td class="Log">
-				
-					<ul>
-					
-						<li><a href="http://ps2dev.org">PS2DEV</a> - Great site for various PS2 hardware information and source code.</li>
-						<li><a href="http://www.pdroms.de/">PDRoms</a> - Nice collection of public domain ROM images.</li>
-					</ul>
-				
-				</td>
-				
-			</tr>
-			
-			<tr>
-			
-				<td class="Title">
-				
-					PSF Music Archives
-					
-				</td>
-				
-			</tr>
-			
-			<tr>
-			
-				<td class="Log">
-				
-					<ul>
-					
-						<li><a href="http://tzone.org/~llin/psf/">PSF Mirror #1</a> - The ultimate PSF/PSF2 collection.</li>
-						<li><a href="http://www.zophar.net/music/psf.html">Zophar's Domain PSF Music Section</a> - Another very nice collection of PlayStation and PlayStation2 music.</li>
-					</ul>
-				
-				</td>
-				
-			</tr>
-
-		</table>
-
 	</body>
 
 </html>
