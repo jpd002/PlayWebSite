fd93022ee7b1b20d879c8aaf3f370b7fee006aa4
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2019-10-22 19:06:55 +0100

    add footer

---
+++ include/footer.include.php
@@ -0,0 +1,21 @@
+<div class="container-fluid">
+	<div class="row bg-primary check-contrast py-4">
+		<div class="col-12 col-md-2 offset-md-4 text-light ">
+			<h5>Github</h5>
+				<a class="text-light" href="https://github.com/jpd002/Play-"><b>Repo</b></a>
+				<br />
+				<a class="text-light" href="https://github.com/jpd002/Play-/graphs/contributors"><b>Contributors</b></a>
+				<br />
+				<a class="text-light" href="https://github.com/jpd002/Play-/pulls"><b>Pull Requests</b></a>
+				<br />
+				<a class="text-light" href="https://github.com/jpd002/Play-Compatibility"><b>Compatibility Tracker</b></a>
+				<br />
+				<a class="text-light" href="https://github.com/jpd002/Play-/issues"><b>Issues</b></a>
+		</div>
+		<div class="col-12 col-md-2 text-light ">
+			<h5>Videos</h5>
+			<a class="text-light" href="https://www.youtube.com/playlist?list=PLph9xgFt_cC1WrjHRx0KI7tkO6sln3_IG"><b>Literalmente{Game}</b></a>
+		</div>
+		<h5 class="col-12 col-md-4 text-light text-right align-self-end">Founder/Author: jpd002 (Jean) Play!</h5>
+	</div>
+</div>
