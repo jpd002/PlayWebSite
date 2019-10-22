3ff21ce974533610d782cc00069473d479b7b66f
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2019-10-22 19:16:17 +0100

    update about.php page

--- about.php
+++ about.php
@@ -9,89 +9,75 @@
 	
 	<body>
 
-		<?php
+	<?php
 			include("navi.php");
 		?>
+		<div class="container-fluid">
 
-		<div>
-			<br />
-			<br />
-		</div>
-
-		<table class="center">
-			<tr>
-				<td class="Header">
-					About
-				</td>
-			</tr>
-			<tr>
-				<td class="Log">
-					<div>
-						Play! is a portable PlayStation2 emulator. The focus of the project is on making PlayStation2 
-						emulation easy and accessible. Just pop in your favorite game's disc and have fun!
-						<ul>
-							<li>No BIOS required. Play! is an high-level emulator.</li>
-							<li>Easy configuration. No custom settings to fix specific games.</li>
-							<li>Available on many platforms. Making sure the experience is the same everywhere.</li>
-						</ul>
+		<div class="row justify-content-center p-4">
+				<div class="col-12 col-lg-8">
+					<div class="jumbotron w-100 py-5 mx-auto">
+						<h1>About</h1>
+						<p class="lead">
+							Play! is a portable PlayStation2 emulator. The focus of the project is on making PlayStation2 
+							emulation easy and accessible. Just pop in your favorite game's disc and have fun!
+							<ul>
+								<li>No BIOS required. Play! is an high-level emulator.</li>
+								<li>Easy configuration. No custom settings to fix specific games.</li>
+								<li>Available on many platforms. Making sure the experience is the same everywhere.</li>
+							</ul>
+						</p>
 					</div>
-				</td>
-			</tr>
-		</table>
+				</div>
+			</div>
 
-		<div>
-			<br />
-		</div>
+			<div class="row justify-content-center p-4">
+				<div class="col-12 col-md-10">
 
-		<table class="center">
-			<tr>
-				<td class="Header">
-					Supporting the project
-				</td>
-			</tr>
-			<tr>
-				<td class="Log">
-					<div>
-						Supporting me financially will allow me to spend more time on the project and improve the quality of the emulator.
-						It's currently possible to contribute through these channels:
+					<div class="card w-100">
+						<div class="card-body">
+							<h5 class="card-title">Supporting the project</h5>
+							<p class="card-text">
+								<div>
+									Supporting me financially will allow me to spend more time on the project and improve the quality of the emulator.
+									It's currently possible to contribute through these channels:
+								</div>
+								<ul>
+									<li>Patreon: <a href="https://www.patreon.com/bePatron?u=7311516" data-patreon-widget-type="become-patron-button">Become a Patron!</a><script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script></li>
+									<li>
+										<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+											<input type="hidden" name="cmd" value="_s-xclick">
+											<input type="hidden" name="hosted_button_id" value="SEFB8MUHNHB4Y">
+											Paypal: <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+											<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+										</form>
+									</li>
+								</ul>
+							</p>
+						</div>
 					</div>
-					<ul>
-						<li>Patreon: <a href="https://www.patreon.com/bePatron?u=7311516" data-patreon-widget-type="become-patron-button">Become a Patron!</a><script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script></li>
-						<li>
-							<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-								<input type="hidden" name="cmd" value="_s-xclick">
-								<input type="hidden" name="hosted_button_id" value="SEFB8MUHNHB4Y">
-								Paypal: <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-								<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-							</form>
-						</li>
-					</ul>
-				</td>
-			</tr>
-		</table>
-		
-		<div>
-			<br />
-		</div>
-		
-		<table class="center">
-			<tr>
-				<td class="Header">
-					Contact Info
-				</td>
-			</tr>
-			<tr>
-				<td class="Log">
-					Author: Jean-Philip Desjardins
-					<br />
-					e-mail: <a href="mailto:jean-philip.desjardins@polymtl.ca">Click Here</a>.
-				</td>
-			</tr>
-		</table>
-		
-		<div>
-			<br />
-		</div>
 
+				</div>
+			</div>
+
+			<div class="row justify-content-center p-4">
+				<div class="col-12 col-md-10">
+
+					<div class="card w-100">
+						<div class="card-body">
+							<h5 class="card-title">Contact Info </h5>
+							<p class="card-text">
+								Author: Jean-Philip Desjardins
+								<br />
+								e-mail: <a href="mailto:jean-philip.desjardins@polymtl.ca">Click Here</a>.
+							</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+		</div>
 	</body>
+
 </html>
