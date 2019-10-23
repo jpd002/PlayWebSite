a9f9177d467db96349ab9e4c0f9133d193e1d513
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2019-10-23 15:44:53 +0100

    update navi.php

--- navi.php
+++ navi.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-primary">
+<nav class="navbar fixed-top navbar-expand-md navbar-dark bg-primary">
 	<a class="navbar-brand" href=".">Play!</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
@@ -6,8 +6,16 @@
 	<div class="collapse navbar-collapse" id="navbarColor02">
 		<ul class="navbar-nav ml-auto">
 			<li class="nav-item">
-				<a class="nav-link" href="https://www.patreon.com/bePatron?u=7311516">Patreon <i class="fab fa-patreon"></i></a>
+				<a class="nav-link" href="https://www.patreon.com/jpd002">Patreon <i class="fab fa-patreon"></i></a>
 			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="https://github.com/jpd002/Play-">Github <i class="fab fa-github"></i></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="https://discord.gg/YG3qRzz">Discord <i class="fab fa-discord"></i></a>
+			</li>
+		</ul>
+		<ul class="navbar-nav mr-auto">
 			<li class="nav-item">
 				<a class="nav-link" href="blog.php">Blog</a>
 			</li>
@@ -21,8 +29,9 @@
 				<a class="nav-link" href="about.php">About</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="screenshots.php">Screen shots</a>
+				<a class="nav-link" href="screenshots.php">Screenshots</a>
 			</li>
 		</ul>
 	</div>
 </nav>
+<div class="mb-5"></div>
