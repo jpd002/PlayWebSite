2829235bdbbd320ff95037e6645666ea39dab66e
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2022-08-16 09:16:46 -0400

    Add basic privacy policy.

---
+++ privacy_policy.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<title>Play! - Privacy Policy</title>
+		<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+		<link rel="stylesheet" type="text/css" href="css/purei.css" />
+		<link rel="stylesheet" type="text/css" href="css/navi.css" />
+	</head>
+	
+	<body>
+
+		<?php
+			include("navi.php");
+		?>
+
+		<div>
+			<br />
+			<br />
+		</div>
+
+		<table class="center">
+			<tr>
+				<td class="Header">
+					Privacy Policy - Web Site
+				</td>
+			</tr>
+			<tr>
+				<td class="Log">
+					<div>
+						Anonymous statistics are collected by this website. No personal information is recorded and cookies are not used.
+					</div>
+				</td>
+			</tr>
+		</table>
+
+		<div>
+			<br />
+		</div>
+
+		<table class="center">
+			<tr>
+				<td class="Header">
+					Privacy Policy - Application
+				</td>
+			</tr>
+			<tr>
+				<td class="Log">
+					<div>
+						The Play! application doesn't collect any information about its users.
+					</div>
+				</td>
+			</tr>
+		</table>
+
+		<div>
+			<br />
+		</div>
+
+	</body>
+</html>
